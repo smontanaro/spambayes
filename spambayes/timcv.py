c5fdca675deba54445bd30aad8c40c15a0cf3753
tim_one <tim_one@users.sourceforge.net>
2002-09-14 22:01:42 +0000

    Introduced new optional arguments to use only part of the ham and spam in each set.  This helps those with larger corpora to run tests as if they had less.

--- spambayes/timcv.py
+++ spambayes/timcv.py
@@ -3,7 +3,7 @@
 
 # A driver for N-fold cross validation.
 
-"""Usage: %(program)s [-h] -n nsets
+"""Usage: %(program)s [options] -n nsets
 
 Where:
     -h
@@ -12,16 +12,36 @@ Where:
         Number of Set directories (Data/Spam/Set1, ... and Data/Ham/Set1, ...).
         This is required.
 
+If you only want to use some of the messages in each set,
+
+    --ham-keep int
+        The maximum number of msgs to use from each Ham set.  The msgs are
+        chosen randomly.  See also the -s option.
+
+    --spam-keep int
+        The maximum number of msgs to use from each Spam set.  The msgs are
+        chosen randomly.  See also the -s option.
+
+    -s int
+        A seed for the random number generator.  Has no effect unless
+        at least on of {--ham-keep, --spam-keep} is specified.  If -s
+        isn't specifed, the seed is taken from current time.
+
 In addition, an attempt is made to merge bayescustomize.ini into the options.
 If that exists, it can be used to change the settings in Options.options.
 """
 
 import os
 import sys
+import random
 
 from Options import options
 from tokenizer import tokenize
-from TestDriver import Driver
+import TestDriver
+
+HAMKEEP  = None
+SPAMKEEP = None
+SEED = random.randrange(2000000000)
 
 program = sys.argv[0]
 
@@ -34,6 +54,8 @@ def usage(code, msg=''):
     sys.exit(code)
 
 class Msg(object):
+    __slots__ = 'tag', 'guts'
+
     def __init__(self, dir, name):
         path = dir + "/" + name
         self.tag = path
@@ -44,6 +66,7 @@ class Msg(object):
     def __iter__(self):
         return tokenize(self.guts)
 
+    # Compare msgs by their paths; this is appropriate for sets of msgs.
     def __hash__(self):
         return hash(self.tag)
 
@@ -54,30 +77,44 @@ class Msg(object):
         return self.guts
 
 class MsgStream(object):
-    def __init__(self, tag, directories):
+    __slots__ = 'tag', 'directories', 'keep'
+
+    def __init__(self, tag, directories, keep=None):
         self.tag = tag
         self.directories = directories
+        self.keep = keep
 
     def __str__(self):
         return self.tag
 
     def produce(self):
-        for directory in self.directories:
-            for fname in os.listdir(directory):
-                yield Msg(directory, fname)
-
-    def xproduce(self):
-        import random
-        keep = 'Spam' in self.directories[0] and 300 or 300
+        if self.keep is None:
+            for directory in self.directories:
+                for fname in os.listdir(directory):
+                    yield Msg(directory, fname)
+            return
+        # We only want part of the msgs.  Shuffle each directory list, but
+        # in such a way that we'll get the same result each time this is
+        # called on the same directory list.
         for directory in self.directories:
             all = os.listdir(directory)
-            random.seed(hash(max(all)) ^ 0x12345678) # reproducible across calls
+            random.seed(hash(max(all)) ^ SEED) # reproducible across calls
             random.shuffle(all)
-            for fname in all[:keep]:
+            del all[self.keep:]
+            all.sort()  # seems to speed access on Win98!
+            for fname in all:
                 yield Msg(directory, fname)
 
     def __iter__(self):
         return self.produce()
+
+class HamStream(MsgStream):
+    def __init__(self, tag, directories):
+        MsgStream.__init__(self, tag, directories, HAMKEEP)
+
+class SpamStream(MsgStream):
+    def __init__(self, tag, directories):
+        MsgStream.__init__(self, tag, directories, SPAMKEEP)
 
 def drive(nsets):
     print options.display()
@@ -85,17 +122,17 @@ def drive(nsets):
     hamdirs  = ["Data/Ham/Set%d" % i for i in range(1, nsets+1)]
     spamdirs = ["Data/Spam/Set%d" % i for i in range(1, nsets+1)]
 
-    d = Driver()
+    d = TestDriver.Driver()
     # Train it on all sets except the first.
-    d.train(MsgStream("%s-%d" % (hamdirs[1], nsets), hamdirs[1:]),
-            MsgStream("%s-%d" % (spamdirs[1], nsets), spamdirs[1:]))
+    d.train(HamStream("%s-%d" % (hamdirs[1], nsets), hamdirs[1:]),
+            SpamStream("%s-%d" % (spamdirs[1], nsets), spamdirs[1:]))
 
     # Now run nsets times, predicting pair i against all except pair i.
     for i in range(nsets):
         h = hamdirs[i]
         s = spamdirs[i]
-        hamstream = MsgStream(h, [h])
-        spamstream = MsgStream(s, [s])
+        hamstream = HamStream(h, [h])
+        spamstream = SpamStream(s, [s])
 
         if i > 0:
             # Forget this set.
@@ -111,24 +148,37 @@ def drive(nsets):
 
     d.alldone()
 
-if __name__ == "__main__":
+def main():
+    global SEED, HAMKEEP, SPAMKEEP
     import getopt
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hn:')
+        opts, args = getopt.getopt(sys.argv[1:], 'hn:s:',
+                                   ['ham-keep=', 'spam-keep='])
     except getopt.error, msg:
         usage(1, msg)
 
-    nsets = None
+    nsets = seed = None
     for opt, arg in opts:
         if opt == '-h':
             usage(0)
         elif opt == '-n':
             nsets = int(arg)
+        elif opt == '-s':
+            seed = int(arg)
+        elif opt == '--ham-keep':
+            HAMKEEP = int(arg)
+        elif opt == '--spam-keep':
+            SPAMKEEP = int(arg)
 
     if args:
         usage(1, "Positional arguments not supported")
     if nsets is None:
         usage(1, "-n is required")
+    if seed is not None:
+        SEED = seed
 
     drive(nsets)
+
+if __name__ == "__main__":
+    main()
