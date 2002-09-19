5dea5cf4926617e3d80a18d303761d89b554868c
gward <gward@users.sourceforge.net>
2002-09-19 23:39:24 +0000

    Moved a big chunk of docs.ht to new file background.ht.

--- website/links.h
+++ website/links.h
@@ -1,5 +1,6 @@
 <h3>SpamBayes</h3>
 <li><a href="index.html">Home page</a>
+<li><a href="background.html">Background</a>
 <li><a href="docs.html">Documentation</a>
 <li><a href="developer.html">Developers</a>
 <li><a href="related.html">Related</a>
