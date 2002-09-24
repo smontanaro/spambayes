d03f7e4688f89fb2e19e9a0ba9124cee4886f6c2
montanaro <montanaro@users.sourceforge.net>
2002-09-24 18:07:17 +0000

    add a bunch more modules and scripts

--- spambayes/setup.py
+++ spambayes/setup.py
@@ -1,7 +1,21 @@
 from distutils.core import setup
 
 setup(
-  name='spambayes',
-  scripts=['unheader.py', 'hammie.py'],
-  py_modules=['classifier', 'tokenizer']
+  name='spambayes', 
+  scripts=['unheader.py',
+           'hammie.py',
+           'loosecksum.py',
+           'timtest.py',
+           'timcv.py',
+           'splitndirs.py',
+           'runtest.sh',
+           'rebal.py',
+           'cmp.py',
+           'rates.py'],
+  py_modules=['classifier',
+              'tokenizer',
+              'Options',
+              'Tester',
+              'TestDriver',
+              'mboxutils']
   )
