a6ae31904a778019849e399031c545953d22c456
tim_one <tim_one@users.sourceforge.net>
2002-10-16 21:07:05 +0000

    chi2Q():  Slight error in the platform exp(), and accumlated roundoff errors, could cause the result of this to spill a few ULP above 1.0, for large x2 and large v.  This can in turn lead to "slightly negative" final spam scores (like -1.2e-15).  Repaired that; negative final scores should never happen now.

--- spambayes/chi2.py
+++ spambayes/chi2.py
@@ -1,17 +1,22 @@
 import math as _math
 
-def chi2Q(x2, v, exp=_math.exp):
+def chi2Q(x2, v, exp=_math.exp, min=min):
     """Return prob(chisq >= x2, with v degrees of freedom).
 
     v must be even.
     """
     assert v & 1 == 0
+    # XXX Is x2 is very large, exp(-m) will underflow to 0.
     m = x2 / 2.0
     sum = term = exp(-m)
     for i in range(1, v//2):
         term *= m / i
         sum += term
-    return sum
+    # With small x2 and large v, accumulated roundoff error, plus error in
+    # the platform exp(), can cause this to spill a few ULP above 1.0.  For
+    # example, chi2Q(100, 300) on my box has sum == 1.0 + 2.0**-52 at this
+    # point.  Returning a value even a teensy bit over 1.0 is no good.
+    return min(sum, 1.0)
 
 def normZ(z, sqrt2pi=_math.sqrt(2.0*_math.pi), exp=_math.exp):
     "Return value of the unit Gaussian at z."
