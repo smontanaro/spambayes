775ce51a0be5b27ac9a0f5a34658264f09779e13
anthonybaxter <anthonybaxter@users.sourceforge.net>
2002-11-01 04:50:19 +0000

    initial 'applications' notes.

--- website/links.h
+++ website/links.h
@@ -2,5 +2,6 @@
 <li><a href="index.html">Home page</a>
 <li><a href="background.html">Background</a>
 <li><a href="docs.html">Documentation</a>
+<li><a href="applications.html">Applications</a>
 <li><a href="developer.html">Developers</a>
 <li><a href="related.html">Related</a>
