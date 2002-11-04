fc0d4f00f71b211e43b7ebe8a0be682cf9a73d8e
mhammond <mhammond@users.sourceforge.net>
2002-11-04 01:12:53 +0000

    Fix the root of my:   File "F:\src\spambayes\classifier.py", line 450, in _getclues     distance = abs(prob - 0.5)
    Exception - problem is that we trained, but didn't update probabilities -
    thus, we failed for every new word seen only since the last complete
    retrain.
    
    There may be a case for _getclues() to detect a probability of None
    and call update_probabilities() automatically - either that or just
    keep throwing vague exceptions <wink>

--- spambayes/Outlook2000/train.py
+++ spambayes/Outlook2000/train.py
@@ -18,7 +18,7 @@ def been_trained_as_spam(msg, mgr):
     # spam is None
     return spam == True
 
-def train_message(msg, is_spam, mgr):
+def train_message(msg, is_spam, mgr, update_probs = True):
     # Train an individual message.
     # Returns True if newly added (message will be correctly
     # untrained if it was in the wrong category), False if already
@@ -40,6 +40,9 @@ def train_message(msg, is_spam, mgr):
     # OK - setup the new data.
     mgr.bayes.learn(tokens, is_spam, False)
     mgr.message_db[msg.searchkey] = is_spam
+    if update_probs:
+        mgr.bayes.update_probabilities()
+
     mgr.bayes_dirty = True
     return True
 
@@ -50,7 +53,7 @@ def train_folder( f, isspam, mgr, progress):
             break
         progress.tick()
         try:
-            if train_message(message, isspam, mgr):
+            if train_message(message, isspam, mgr, False):
                 num_added += 1
         except:
             print "Error training message '%s'" % (message,)
