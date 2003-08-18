60bc3bd526bee6f73e50d2f026ef682994fb7205
mhammond <mhammond@users.sourceforge.net>
2003-08-18 04:25:03 +0000

    Fix paths yet again in the test code.

--- spambayes/Outlook2000/dialogs/test_dialogs.py
+++ spambayes/Outlook2000/dialogs/test_dialogs.py
@@ -1,11 +1,14 @@
 # Test code for the SpamBayes dialogs.
+import sys, os
 if __name__=='__main__':
     # Hack for testing - setup sys.path
     try:
-        import spambayes.Options
+        import spambayes.Version
     except ImportError:
-        import sys, os
         sys.path.append(os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), "..", "..")))
+    try:
+        import manager
+    except ImportError:
         sys.path.append(os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), "..")))
     
     import manager
