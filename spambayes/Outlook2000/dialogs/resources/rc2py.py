117f2e17d1cbd5019f4320fdef8b165688c7388d
mhammond <mhammond@users.sourceforge.net>
2003-08-26 10:57:44 +0000

    Write the size and time of the .rc file, and don't bother deriving from object.

--- spambayes/Outlook2000/dialogs/resources/rc2py.py
+++ spambayes/Outlook2000/dialogs/resources/rc2py.py
@@ -7,7 +7,7 @@ __doc__=""""
 Converts an .rc windows resource source file into a python source file
 with the same basic public interface as the rcparser module.
 """
-import sys, os
+import sys, os, stat
 import rcparser
 
 def convert(inputFilename = None, outputFilename = None):
@@ -17,11 +17,13 @@ def convert(inputFilename = None, outputFilename = None):
     if outputFilename is None:
         outputFilename = "test.py"
     rcp = rcparser.ParseDialogs(inputFilename)
+    in_stat = os.stat(inputFilename)
 
     out = open(outputFilename, "wt")
     out.write("#%s\n" % outputFilename)
     out.write("#This is a generated file. Please edit %s instead.\n" % inputFilename)
-    out.write("class FakeParser(object):\n")
+    out.write("_rc_size_=%d\n_rc_mtime_=%d\n" % (in_stat[stat.ST_SIZE], in_stat[stat.ST_MTIME]))
+    out.write("class FakeParser:\n")
     out.write("\tdialogs = "+repr(rcp.dialogs)+"\n")
     out.write("\tids = "+repr(rcp.ids)+"\n")
     out.write("\tnames = "+repr(rcp.names)+"\n")
