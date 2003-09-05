8f1653cb36a2a2638382cd9d30d75597882358ff
mhammond <mhammond@users.sourceforge.net>
2003-09-05 06:53:24 +0000

    Add GetImageParamsFromBitmapID() to centralize bitmap location logic, hopefully taking into account a py2exe future.

--- spambayes/Outlook2000/dialogs/resources/__init__.py
+++ spambayes/Outlook2000/dialogs/resources/__init__.py
@@ -1,1 +1,22 @@
-# Empty file to designate this directory as a module.
+# Package that manages and defines dialog resources
+
+def GetImageParamsFromBitmapID(rc_parser, bmpid):
+    import os, sys
+    import win32gui, win32con, win32api
+    if type(bmpid)==type(0):
+        bmpid = rc_parser.names[bmpid]
+    # For both binary and source versions, we currently load from files.
+    # In future py2exe built binary versions we will be able to load the
+    # bitmaps directly from our DLL.
+    filename = rc_parser.bitmaps[bmpid]
+    if hasattr(sys, "frozen"):
+        # bitmap in the app/images directory
+        # dont have manager available :(
+        dll_filename = win32api.GetModuleFileName(sys.frozendllhandle)
+        app_dir = os.path.dirname(dll_filename)
+        filename = os.path.join(app_dir, "images", filename)
+    else:
+        if not os.path.isabs(filename):
+            # In this directory
+            filename = os.path.join( os.path.dirname( __file__ ), filename)
+    return 0, filename, win32con.LR_LOADFROMFILE
