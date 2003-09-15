1378c15a0fdcf68a47752dcab5154913e8ee0f06
mhammond <mhammond@users.sourceforge.net>
2003-09-15 06:27:09 +0000

    Load dialog bitmaps directly from the executable in binary builds.

--- spambayes/Outlook2000/dialogs/resources/__init__.py
+++ spambayes/Outlook2000/dialogs/resources/__init__.py
@@ -5,18 +5,22 @@ def GetImageParamsFromBitmapID(rc_parser, bmpid):
     import win32gui, win32con, win32api
     if type(bmpid)==type(0):
         bmpid = rc_parser.names[bmpid]
+    int_bmpid = rc_parser.ids[bmpid]
     # For both binary and source versions, we currently load from files.
     # In future py2exe built binary versions we will be able to load the
     # bitmaps directly from our DLL.
     filename = rc_parser.bitmaps[bmpid]
     if hasattr(sys, "frozen"):
-        # bitmap in the app/images directory
-        # dont have manager available :(
-        dll_filename = win32api.GetModuleFileName(sys.frozendllhandle)
-        app_dir = os.path.dirname(dll_filename)
-        filename = os.path.join(app_dir, "images", filename)
+        # in our .exe/.dll - load from that.
+        if sys.frozen=="dll":
+            hmod = sys.frozendllhandle
+        else:
+            hmod = win32api.GetModuleHandle(None)
+        return hmod, int_bmpid, 0
     else:
+        # source code - load the .bmp directly.
         if not os.path.isabs(filename):
             # In this directory
             filename = os.path.join( os.path.dirname( __file__ ), filename)
-    return 0, filename, win32con.LR_LOADFROMFILE
+        return 0, filename, win32con.LR_LOADFROMFILE
+    assert 0, "not reached"
