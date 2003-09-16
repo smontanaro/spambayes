4aaa0bc63efddacb2cf1b6918734cb11e849dadf
mhammond <mhammond@users.sourceforge.net>
2003-09-16 14:01:47 +0000

    Only munge sys.path in source-code versions, and as the service starts, have it report the username and ini file it is using. In binary builds, write a log to %temp%\SpamBayesServicen.log

--- spambayes/windows/pop3proxy_service.py
+++ spambayes/windows/pop3proxy_service.py
@@ -17,30 +17,61 @@
 # Originally written by Mark Hammond.
 
 import sys, os
-# We are in the 'spambayes\win32' directory.  We
-# need the parent on sys.path, so 'spambayes.spambayes' is a package,
-# and 'pop3proxy' is a module
+
+# Messages from pop3proxy will go nowhere when executed as a service
+# Try and detect that print will go nowhere and redirect.
+# redirect output somewhere useful when running as a service.
+import win32api
 try:
-    # module imported by service manager, or 2.3 (in which __main__
-    # exists, *and* sys.argv[0] is always already absolute)
-    this_filename=__file__
-except NameError:
-    this_filename = sys.argv[0]
-if not os.path.isabs(sys.argv[0]):
-    # Python 2.3 __main__
-    # patch up sys.argv, as our cwd will confuse service registration code
-    sys.argv[0] = os.path.abspath(sys.argv[0])
-    this_filename = sys.argv[0]
+    win32api.GetConsoleTitle()
+except win32api.error:
+    # No console - if we are running from Python sources,
+    # redirect to win32traceutil, but if running from a binary
+    # install, redirect to a log file.
+    # Want to move to logging module later, so for now, we
+    # hack together a simple logging strategy.
+    if hasattr(sys, "frozen"):
+        temp_dir = win32api.GetTempPath()
+        for i in range(3,0,-1):
+            try: os.unlink(os.path.join(temp_dir, "SpamBayesService%d.log" % (i+1)))
+            except os.error: pass
+            try:
+                os.rename(
+                    os.path.join(temp_dir, "SpamBayesService%d.log" % i),
+                    os.path.join(temp_dir, "SpamBayesService%d.log" % (i+1))
+                    )
+            except os.error: pass
+        # Open this log, as unbuffered so crashes still get written.
+        sys.stdout = open(os.path.join(temp_dir,"SpamBayesService1.log"), "wt", 0)
+        sys.stderr = sys.stdout
+    else:
+        import win32traceutil
 
-print sys.argv[0]
-sb_dir = os.path.dirname(os.path.dirname(this_filename))
-sb_scripts_dir = os.path.join(sb_dir,"scripts")
+# If running from sources, patch up sys.path
+if not hasattr(sys, "frozen"):
+    # We are in the 'spambayes\win32' directory.  We
+    # need the parent on sys.path, so 'spambayes.spambayes' is a package,
+    # and 'pop3proxy' is a module
+    try:
+        # module imported by service manager, or 2.3 (in which __main__
+        # exists, *and* sys.argv[0] is always already absolute)
+        this_filename=__file__
+    except NameError:
+        this_filename = sys.argv[0]
+    if not os.path.isabs(sys.argv[0]):
+        # Python 2.3 __main__
+        # patch up sys.argv, as our cwd will confuse service registration code
+        sys.argv[0] = os.path.abspath(sys.argv[0])
+        this_filename = sys.argv[0]
 
-sys.path.insert(0, sb_dir)
-sys.path.insert(-1, sb_scripts_dir)
-# and change directory here, so pop3proxy uses the default
-# config file etc
-os.chdir(sb_dir)
+    sb_dir = os.path.dirname(os.path.dirname(this_filename))
+    sb_scripts_dir = os.path.join(sb_dir,"scripts")
+    
+    sys.path.insert(0, sb_dir)
+    sys.path.insert(-1, sb_scripts_dir)
+    # and change directory here, so pop3proxy uses the default
+    # config file etc
+    os.chdir(sb_dir)
 
 # Rest of the standard Python modules we use.
 import traceback
@@ -53,23 +84,7 @@ import sb_server
 # The win32 specific modules.
 import win32serviceutil, win32service
 import pywintypes, win32con, winerror
-
 from ntsecuritycon import *
-
-# Messages from pop3proxy will go nowhere when executed as a service
-# Try and detect that print will go nowhere and redirect.
-try:
-    # redirect output somewhere useful when running as a service.
-    import win32api
-    try:
-        win32api.GetConsoleTitle()
-    except win32api.error:
-        # no console - import win32traceutil
-        import win32traceutil
-        print "popproxy service module loading (as user %s)..." \
-                % win32api.GetUserName()
-except ImportError:
-    pass
 
 class Service(win32serviceutil.ServiceFramework):
     # The script name was changed to "sb_server" but I'll leave this as pop3proxy
@@ -87,7 +102,6 @@ class Service(win32serviceutil.ServiceFramework):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
         self.event_stopping.set()
         sb_server.stop(sb_server.state)        
-        
 
     def SvcDoRun(self):
         # Setup our state etc
@@ -100,11 +114,15 @@ class Service(win32serviceutil.ServiceFramework):
 
         # Write an event log record - in debug mode we will also 
         # see this message printed.
+        from spambayes.Options import optionsPathname
+        extra = " as user '%s', using config file '%s'" \
+                % (win32api.GetUserNameEx(2).encode("mbcs"),
+                   optionsPathname.encode("mbcs"))
         import servicemanager
         servicemanager.LogMsg(
             servicemanager.EVENTLOG_INFORMATION_TYPE,
             servicemanager.PYS_SERVICE_STARTED,
-            (self._svc_name_, '')
+            (self._svc_name_, extra)
             )
 
         try:
