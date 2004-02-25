5643352241f05367bc81d9ed541364aa834fe70f
montanaro <montanaro@users.sourceforge.net>
2004-02-25 22:19:43 +0000

    Add a -c flag.  "-c ext" is used to trigger writing of a new pair of ham and spam files (both unix mbox files) which contain just the messages which were trained on at least once during the run.  This can be used to cull useless messages from the data sets.

--- spambayes/contrib/tte.py
+++ spambayes/contrib/tte.py
@@ -4,24 +4,38 @@
 Train to exhaustion: train repeatedly on a pile of ham and spam until
 everything scores properly.
 
-usage %(prog)s [ -h ] -g file -s file [ -d file | -p file ] [ -m N ] [ -r N ]
+usage %(prog)s [ -h ] -g file -s file [ -d file | -p file ] [ -m N ] [ -r N ] [ -c ext ]
 
--h      - print this documentation and exit.
+-h      - Print this usage message and exit.
 
--g file - take ham from file
+-g file - Take ham from file.
 
--s file - take spam from file
+-s file - Take spam from file.
 
--d file - use a database-based classifier named file
+-d file - Use a database-based classifier named file.
 
--p file - use a pickle-based classifier named file
+-p file - Use a pickle-based classifier named file.
 
--m N    - train on at most N messages (nham == N/2 and nspam == N/2)
+-m N    - Train on at most N messages (nham == N/2 and nspam == N/2).
 
--r N    - run at most N rounds (default %(MAXROUNDS)s), even if not
-          all messages score correctly
+-r N    - Run at most N rounds (default %(MAXROUNDS)s), even if not
+          all messages score correctly.
 
-See Gary Robinson's blog:
+-c ext  - Cull all messages which aren't used as training input during any run
+          and write to new ham and spam files with ext as an extra file extension.
+          All messages which are never considered (because one training set is
+          longer than the other or the -m flag was used to reduce the amount of
+          input) are retained.
+
+Note that the -c command line argument isn't quite as benign as it might
+first appear.  Since the tte protocol trains on the same number of ham and
+spam messages, if you use the output of one run as input into a later run
+you will almost certainly train on fewer messages than before since the two
+files will probably not have the same number of messages.  The extra
+messages in the longer file will be ignored in future runs until you add
+more messages to the shorter file.
+
+For more detail on the notion of training to exhaustion see Gary Robinson's blog:
 
     http://www.garyrobinson.net/2004/02/spam_filtering_.html
 """
@@ -47,7 +61,7 @@ def usage(msg=None):
         print >> sys.stderr, msg
     print >> sys.stderr, __doc__.strip() % globals()
 
-def train(store, ham, spam, maxmsgs, maxrounds):
+def train(store, ham, spam, maxmsgs, maxrounds, tdict):
     smisses = hmisses = round = 0
     ham_cutoff = Options.options["Categorization", "ham_cutoff"]
     spam_cutoff = Options.options["Categorization", "spam_cutoff"]
@@ -69,10 +83,12 @@ def train(store, ham, spam, maxmsgs, maxrounds):
 
                 if store.spamprob(tokenize(hammsg)) > ham_cutoff:
                     hmisses += 1
+                    tdict[hammsg["message-id"]] = True
                     store.learn(tokenize(hammsg), False)
 
                 if store.spamprob(tokenize(spammsg)) < spam_cutoff:
                     smisses += 1
+                    tdict[spammsg["message-id"]] = True
                     store.learn(tokenize(spammsg), True)
 
         except StopIteration:
@@ -84,10 +100,14 @@ def train(store, ham, spam, maxmsgs, maxrounds):
         print "\rround: %2d, msgs: %4d, ham misses: %3d, spam misses: %3d, %.1fs" % \
               (round, nmsgs, hmisses, smisses, seconds)
 
+    # We count all untrained messages so the user knows what was skipped.
+    # We also tag them for saving so we don't lose messages which might have
+    # value in a future run
     nhamleft = 0
     try:
         while True:
-            hambone.next()
+            msg = hambone.next()
+            tdict[msg["message-id"]] = True
             nhamleft += 1
     except StopIteration:
         if nhamleft: print nhamleft, "untrained hams"
@@ -95,22 +115,24 @@ def train(store, ham, spam, maxmsgs, maxrounds):
     nspamleft = 0
     try:
         while True:
-            spamcan.next()
+            msg = spamcan.next()
+            tdict[msg["message-id"]] = True
             nspamleft += 1
     except StopIteration:
         if nspamleft: print nspamleft, "untrained spams"
 
 def main(args):
     try:
-        opts, args = getopt.getopt(args, "hg:s:d:p:o:m:r:",
+        opts, args = getopt.getopt(args, "hg:s:d:p:o:m:r:c:",
                                    ["help", "good=", "spam=",
                                     "database=", "pickle=",
-                                    "option=", "max=", "maxrounds="])
+                                    "option=", "max=", "maxrounds=",
+                                    "cullext="])
     except getopt.GetoptError, msg:
         usage(msg)
         return 1
 
-    ham = spam = dbname = usedb = None
+    ham = spam = dbname = usedb = cullext = None
     maxmsgs = 0
     maxrounds = MAXROUNDS
     for opt, arg in opts:
@@ -121,6 +143,8 @@ def main(args):
             ham = arg
         elif opt in ("-s", "--spam"):
             spam = arg
+        elif opt in ("-c", "--cullext"):
+            cullext = arg
         elif opt in ("-m", "--max"):
             maxmsgs = int(arg)
         elif opt in ("-r", "--maxrounds"):
@@ -141,9 +165,37 @@ def main(args):
 
     store = storage.open_storage(dbname, usedb)
 
-    train(store, ham, spam, maxmsgs, maxrounds)
+    tdict = {}
+    train(store, ham, spam, maxmsgs, maxrounds, tdict)
 
     store.store()
+
+    if cullext is not None:
+        print "writing new ham mbox..."
+        n = m = 0
+        newham = file(ham + cullext, "w")
+        for msg in mboxutils.getmbox(ham):
+            m += 1
+            if msg["message-id"] in tdict:
+                newham.write(str(msg))
+                n += 1
+            sys.stdout.write("\r%5d of %5d" % (n, m))
+            sys.stdout.flush()
+        sys.stdout.write("\n")
+        newham.close()
+
+        print "writing new spam mbox..."
+        n = m = 0
+        newspam = file(spam + cullext, "w")
+        for msg in mboxutils.getmbox(spam):
+            m += 1
+            if msg["message-id"] in tdict:
+                newspam.write(str(msg))
+                n += 1
+            sys.stdout.write("\r%5d of %5d" % (n, m))
+            sys.stdout.flush()
+        sys.stdout.write("\n")
+        newspam.close()
 
     return 0
 
