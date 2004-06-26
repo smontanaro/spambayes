63463d3153da0c2210e3e13dc9b577454945ada4
montanaro <montanaro@users.sourceforge.net>
2004-06-26 21:07:37 +0000

    Correct the import of smtpd.  It's probably already imported as __main__. We don't want to import it as smtpd in that case because the main program setup may set the DEBUGSTREAM module global.

--- spambayes/spambayes/postfixproxy.py
+++ spambayes/spambayes/postfixproxy.py
@@ -7,7 +7,15 @@ For details, see <http://www.postfix.org/SMTPD_PROXY_README.html>.
 """
 
 import os
-import smtpd
+# This module is generally going to be imported from smtpd which will be run
+# as the main program and thus be named __main__.  Importing smtpd directly
+# will override any global object definitions.  In particular, the
+# DEBUGSTREAM setting might get hosed.
+import __main__ as smtpd
+if not hasattr(smtpd, "PureProxy"):
+    import smtpd
+
+import time
 import email.Parser
 from spambayes import Options, hammie, storage
 
@@ -59,18 +67,27 @@ class SpambayesProxy(smtpd.PureProxy):
         pass
 
     def process_message(self, peer, mailfrom, rcpttos, data):
+        t1 = time.time()
         try:
             msg = email.Parser.Parser().parsestr(data)
         except:
             pass
         else:
+            t2 = time.time()
             msg.add_header("X-Peer", peer[0])
             prob, data = self.h.score_and_filter(msg)
-            if prob >= self.spam_cutoff:
-                self.push('503 Error: probable spam')
-                self.log_message(data)
-                return
+            t3 = time.time()
+            try:
+                if prob >= self.spam_cutoff:
+                    self.log_message(data)
+                    print >> smtpd.DEBUGSTREAM, 'probable spam: %.2f' % prob
+                    return '503 Error: probable spam'
 
-        refused = self._deliver(mailfrom, rcpttos, data)
-        # TBD: what to do with refused addresses?
-        print >> smtpd.DEBUGSTREAM, 'we got some refusals:', refused
+                refused = self._deliver(mailfrom, rcpttos, data)
+                t4 = time.time()
+                # TBD: what to do with refused addresses?
+                print >> smtpd.DEBUGSTREAM, 'we got some refusals:', refused
+                print >> smtpd.DEBUGSTREAM, 'deliver time:', t4-t3
+            finally:
+                print >> smtpd.DEBUGSTREAM, 'parse time:', t2-t1
+                print >> smtpd.DEBUGSTREAM, 'score time:', t3-t2
