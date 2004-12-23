f34fa1a08895350f09adafab4decdb863d6c4d91
kpitt <kpitt@users.sourceforge.net>
2004-12-23 17:13:37 +0000

    Update the _APS_NEXT_CONTROL_VALUE so that we don't get an id conflict the next time someone adds a control from the Visual Studio resource editor.

--- spambayes/Outlook2000/dialogs/resources/dialogs.h
+++ spambayes/Outlook2000/dialogs/resources/dialogs.h
@@ -111,7 +111,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        128
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1098
+#define _APS_NEXT_CONTROL_VALUE         1100
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
