a0504c5d37787d8d0c5cc63c38b7b31e82ba3db2
anadelonbrin <anadelonbrin@users.sourceforge.net>
2005-01-02 04:55:46 +0000

    Write a handler incase gettext is not available.
    To convert a '0' or '1' to a bool, it must be an int first.

--- spambayes/Outlook2000/dialogs/resources/rc2py.py
+++ spambayes/Outlook2000/dialogs/resources/rc2py.py
@@ -24,18 +24,19 @@ def convert(inputFilename = None, outputFilename = None,
     out.write("#%s\n" % outputFilename)
     out.write("#This is a generated file. Please edit %s instead.\n" % inputFilename)
     out.write("_rc_size_=%d\n_rc_mtime_=%d\n" % (in_stat[stat.ST_SIZE], in_stat[stat.ST_MTIME]))
+    out.write("try:\n    _\nexcept NameError:\n    def _(s):\n        return s\n")
     out.write("class FakeParser:\n")
-    out.write("\tdialogs = "+repr(rcp.dialogs)+"\n")
-    out.write("\tids = "+repr(rcp.ids)+"\n")
-    out.write("\tnames = "+repr(rcp.names)+"\n")
-    out.write("\tbitmaps = "+repr(rcp.bitmaps)+"\n")
+    out.write("    dialogs = "+repr(rcp.dialogs)+"\n")
+    out.write("    ids = "+repr(rcp.ids)+"\n")
+    out.write("    names = "+repr(rcp.names)+"\n")
+    out.write("    bitmaps = "+repr(rcp.bitmaps)+"\n")
     out.write("def ParseDialogs(s):\n")
-    out.write("\treturn FakeParser()\n")
+    out.write("    return FakeParser()\n")
     out.close()
 
 if __name__=="__main__":
     if len(sys.argv) > 3:
-        convert(sys.argv[1], sys.argv[2], bool(sys.argv[3]))
+        convert(sys.argv[1], sys.argv[2], bool(int(sys.argv[3])))
     elif len(sys.argv) > 2:
         convert(sys.argv[1], sys.argv[2], True)
     else:
