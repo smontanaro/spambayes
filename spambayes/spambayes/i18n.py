66d2e5b8d2f1a70564600bcd7777f4a5f8983efc
anadelonbrin <anadelonbrin@users.sourceforge.net>
2005-04-07 04:19:45 +0000

    Get things working when we are running frozen.
    Basically this handles the move where the languages directory became a subpackage
     of spambayes.

--- spambayes/spambayes/i18n.py
+++ spambayes/spambayes/i18n.py
@@ -29,36 +29,75 @@ from gettext import translation, NullTranslations
 
 ## Set language environment for gettext and for dynamic load of dialogs.
 ##
-## Our directory layout is:
+## Our directory layout with source is:
 ##   spambayes
-##       spambayes
-##           i18n.py             <--- this file
-##       languages               <--- the directory for lang packs
-##           es                  <-- generic language data
-##               DIALOGS
-##               LC_MESSAGES
-##               __init__.py     <-- resourcepackage __init__.py
-##              ui.html          <-- web interface translation
-##           es_ES               <-- specific language/country data.
-##               DIALOGS         <-- resource dialogs
-##               LC_MESSAGES     <-- gettext messages files
-##               __init__.py     <-- resourcepackage __init__.py
-##              ui.html          <-- web interface translation
-##           zn
-##           zn_TW
+##       spambayes (or spambayes.modules with the binary)
+##           i18n.py                 <-- this file
+##           languages               <-- the directory for lang packs
+##               es                  <-- generic language data
+##                   DIALOGS
+##                   LC_MESSAGES     <-- gettext message files
+##                   __init__.py     <-- resourcepackage __init__.py
+##                  ui.html          <-- web interface translation
+##               es_ES               <-- specific language/country data.
+##                   DIALOGS         <-- resource dialogs
+##                   LC_MESSAGES     <-- gettext message files
+##                   __init__.py     <-- resourcepackage __init__.py
+##                  ui.html          <-- web interface translation
+##               zn
+##               zn_TW
 ##       Outlook2000
 ##       utilities
 ##       ..etc..
+##
+## Our directory layout with the binaries is:
+##   lib
+##       spambayes.modules
+##           i18n.py                 <-- this file
+##           languages               <-- the directory for lang packs
+##               es                  <-- generic language data
+##                   DIALOGS
+##                   __init__.py     <-- resourcepackage __init__.py
+##                  ui.html          <-- web interface translation
+##               es_ES               <-- specific language/country data.
+##                   DIALOGS         <-- resource dialogs
+##                   __init__.py     <-- resourcepackage __init__.py
+##                  ui.html          <-- web interface translation
+##               zn
+##               zn_TW
+##       languages
+##           es                  <-- generic language data
+##               LC_MESSAGES     <-- gettext message files
+##           es_ES               <-- specific language/country data.
+##               LC_MESSAGES     <-- gettext message files
+##           zn
+##           zn_TW
+##       ..etc..
+##
+## A distutils installation will not currently work.  I don't know
+## where to put the .mo files, so setup.py ignores them.
 
+if hasattr(sys, "frozen"):
+    # LC_MESSAGES are harder to find.
+    if sys.frozen == "dll":
+        # Outlook
+        import win32api
+        this_filename = win32api.GetModuleFileName(sys.frozendllhandle)
+    else:
+        # Not Outlook
+        this_filename = __file__
+    LC_DIR = os.path.dirname(os.path.dirname(this_filename))
+else:
+    try:
+        this_filename = os.path.abspath(__file__)
+    except NameError: # no __file__ - means Py2.2 and __name__=='__main__'
+        this_filename = os.path.abspath(sys.argv[0])
+    LC_DIR = os.path.dirname(this_filename)
+    
 
 class LanguageManager:
-    def __init__(self, directory=os.path.dirname(__file__)):
-        """Initialisation.
-
-        'directory' is the parent directory of the 'languages'
-        directory.  It defaults to the directory of this module."""
+    def __init__(self):
         self.current_langs_codes = []
-        self.local_dir = os.path.join(directory, "..", "languages")
         self._sys_path_modifications = []
 
     def set_language(self, lang_code=None):
@@ -98,9 +137,9 @@ class LanguageManager:
         """Load and return the appropriate ui_html.py module for the
         current language."""
         for language in self.current_langs_codes:
-            moduleName = 'languages.%s.i18n_ui_html' % (language, )
+            moduleName = 'spambayes.languages.%s.i18n_ui_html' % (language, )
             try:
-                module = __import__(moduleName, {}, {}, ('languages',
+                module = __import__(moduleName, {}, {}, ('spambayes.languages',
                                                          language))
             except ImportError:
                 # That language isn't available - fall back to the
@@ -114,7 +153,7 @@ class LanguageManager:
 
     def _install_gettext(self):
         """Set the gettext specific environment."""
-        lang = translation("messages", self.local_dir,
+        lang = translation("messages", LC_DIR,
                             self.current_langs_codes, fallback=True)
         lang.install()
 
@@ -126,10 +165,9 @@ class LanguageManager:
         code only, so we can simulate the fallback procedures."""
         self._clear_syspath()
         for lcode in self.current_langs_codes:
-            code_and_country = os.path.join(self.local_dir, lcode,
+            code_and_country = os.path.join(LC_DIR, "languages", lcode,
                                             'DIALOGS')
-            code_only = os.path.join(self.local_dir, lcode.split("_")[0],
-                                     'DIALOGS')
+            code_only = os.path.join(LC_DIR, lcode.split("_")[0], 'DIALOGS')
             if code_and_country not in sys.path:
                 sys.path.append(code_and_country)
                 self._sys_path_modifications.append(code_and_country)
