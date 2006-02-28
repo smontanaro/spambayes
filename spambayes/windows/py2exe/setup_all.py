80a71707f8402731bb45899a1d7b678d163c7365
anadelonbrin <anadelonbrin@users.sourceforge.net>
2006-02-28 08:11:40 +0000

    py2exe 0.6.4 introduces a new modulefinder, so our package paths have to be added to that, rather than the built-in modulefinder.  If 0.6.4 isn't installed, then  just use the old system.

--- spambayes/windows/py2exe/setup_all.py
+++ spambayes/windows/py2exe/setup_all.py
@@ -18,7 +18,14 @@ dialogs.LoadDialogs()
 # ModuleFinder can't handle runtime changes to __path__, but win32com uses them,
 # particularly for people who build from sources.  Hook this in.
 try:
-    import modulefinder
+    # py2exe 0.6.4 introduced a replacement modulefinder.
+    # This means we have to add package paths there, not to the built-in
+    # one.  If this new modulefinder gets integrated into Python, then
+    # we might be able to revert this some day.
+    try:
+        import py2exe.mf as modulefinder
+    except ImportError:
+        import modulefinder
     import win32com
     for p in win32com.__path__[1:]:
         modulefinder.AddPackagePath("win32com", p)
